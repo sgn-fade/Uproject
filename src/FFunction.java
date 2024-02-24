03671ea04c024f3a0e5696ae7447ac5657176352
fade <dimabreusgo@gmail.com>
2024-02-24 21:47:26 +0200

    function realisation

---
+++ src/FFunction.java
@@ -0,0 +1,24 @@
+public class FFunction implements Evaluatable {
+    private double a;
+
+    public double getA() {
+        return a;
+    }
+
+    public void setA(double a) {
+        this.a = a;
+    }
+    public FFunction() {
+        this(1.0);
+    }
+    public FFunction(double a) {
+        this.a = a;
+    }
+
+    @Override
+    public double evalf(double x) {
+        return Math.exp(-a*x*x)*Math.sin(x);
+    }
+    public static void main(String[] args) {
+    }
+}
---
+++ src/ListInterpolation.java
@@ -0,0 +1,79 @@
+import java.util.*;
+public class ListInterpolation extends Interpolator {
+    private List<Point2D> data = null;
+    public ListInterpolation(List<Point2D> data) {
+        this.data = data;
+    }
+    public ListInterpolation() {
+        data = new ArrayList<Point2D>();
+    }
+    public ListInterpolation(Point2D[] data) {
+        this();
+        for (Point2D pt : data)
+            this.data.add(pt);
+    }
+    @Override
+    public void clear() {
+        data.clear();
+    }
+    @Override
+    public int numPoints() {
+        return data.size();
+    }
+    @Override
+    public void addPoint(Point2D pt) {
+        data.add(pt);
+    }
+    @Override
+    public Point2D getPoint(int i) {
+        return data.get(i);
+    }
+    @Override
+    public void setPoint(int i, Point2D pt) {
+        data.set(i, pt);
+    }
+    @Override
+    public void removeLastPoint() {
+        data.remove(data.size()-1);
+    }
+    @Override
+    public void sort() {
+        java.util.Collections.sort(data);
+    }
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        ListInterpolation fun = new ListInterpolation();
+        int num;
+        double x;
+        java.util.Scanner in = new java.util.Scanner(System.in);
+        do {
+            System.out.print("Кількість точок: ");
+            num = in.nextInt();
+        } while (num <= 0);
+        for (int i = 0; i < num; i++)
+        {
+            x = 1.0 + (5.0 - 1.0)*Math.random();
+            fun.addPoint(new Point2D(x, Math.sin(x)));
+        }
+        System.out.println("Інтерполяція по: " + fun.numPoints() + " точкам");
+        System.out.println("Несортований набір: ");
+        for (int i = 0; i < fun.numPoints(); i++)
+            System.out.println("Точка " + (i+1) + ": " + fun.getPoint(i));
+        fun.sort();
+        System.out.println("Відсортований набір: ");
+        for (int i = 0; i < fun.numPoints(); i++)
+            System.out.println("Точка " + (i+1) + ": " + fun.getPoint(i));
+        System.out.println("Мінімальне значення x: " + fun.getPoint(0).getX());
+        System.out.println("Максимальне значення x: " +
+                fun.getPoint(fun.numPoints()-1).getX());
+        x = 0.5*(fun.getPoint(0).getX() +
+                fun.getPoint(fun.numPoints()-1).getX());
+        System.out.println("Значення інтерполяції fun(" + x + ") = " +
+                fun.evalf(x));
+        System.out.println("Точне значення sin(" + x + ") = " + Math.sin(x));
+        System.out.println("Абсолютна помилка = " +
+                Math.abs(fun.evalf(x)-Math.sin(x)));
+    }
+}
